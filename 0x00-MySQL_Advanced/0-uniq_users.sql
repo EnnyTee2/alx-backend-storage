1456d577c9066aaa541833cbc0d468de5ddef716
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-13 12:18:26 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- Script to create table 'users' with some requirements
-
+-- id, name, email
 CREATE TABLE IF NOT EXISTS users (
-    PRIMARY KEY(id),
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL
     name VARCHAR(255)
 );
