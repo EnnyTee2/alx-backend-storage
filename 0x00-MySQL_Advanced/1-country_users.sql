e56fa51b08484dfdb395a178be2cca0ad5a4d169
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-13 14:16:20 +0100

    Create 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a table users
+-- id, email, name, country
+
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
