e634671b3ebd4d6e780e5f859773403b0e403ccb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-13 16:19:17 +0100

    Update 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER reset_attr BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF NEW.email != OLD.email THEN
-		UPDATE users SET NEW.valid_email = 0;
+		SET NEW.valid_email = 0;
 	END IF;
 END;$$
 DELIMITER ;
