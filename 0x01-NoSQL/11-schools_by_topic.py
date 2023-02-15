afc59edf366afbdf6670ce3b80574feb5a5083de
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-15 01:56:51 +0100

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,4 +7,4 @@ def schools_by_topic(mongo_collection, topic):
     returns the list of school having a specific topic:
     topic (string) will be the topic searched
     """
-    return [school for school in mongo_collection.find( {topic: topic} )]
+    return [school for school in mongo_collection.find( {topics: topic} )]
