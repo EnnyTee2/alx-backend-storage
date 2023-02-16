6ee90abcfe65f27d808d7ea78c6fce5bab54f3b9
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-16 04:08:28 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,19 +12,20 @@ db = redis.Redis()
 def url_access_counter(funct: Callable) -> Callable:
     """ Decorator counting number of times
     a URL is accessed """
+
     @wraps(funct)
     def wrapper(url):
         db_cache_key = "cached:" + url
+        counter_key = "count:" + url
+        db.incr(counter_key)
         cached_data = db.get(db_cache_key)
         if cached_data:
             return cached_data.decode("utf-8")
 
-        counter_key = "count:" + url
         html_file = funct(url)
-        db.incr(counter_key)
-        db.set(db_cache_key, html_file)
-        db.expire(db_cache_key, 10)
+        db.setex(db_cache_key, 10, html)
         return html_file
+
     return wrapper
 
 
