15af6983d039d761ce332675bda7d1c453bd5c60
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2023-02-16 02:59:52 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
-    def store(data) -> Union[str, bytes, int, float]:
+    def store(data: Union[str, bytes, int, float]) -> str:
         """ store data in redis and return id"""
         data_id = str(uuid())
         self._redis.set(data_id, data)
